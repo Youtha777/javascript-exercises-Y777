e4004694655f9bb87aa5dbbf8b05f8d4a21b5277
Youtah <61557913+Youtha777@users.noreply.github.com>
2026-06-30 09:35:33 -0700

    Completed foundation excercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  //Math.round (num * 10)/10 -- this will round the number in the ones placed and remove trailing 0
+  //conversion formula to fahrenheit -- (num - 32) * 5/9
+  return Math.round((((fahrenheit - 32)) * 5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  //Math.round (num * 10)/10 -- this will round the number in the ones placed and remove trailing 0
+  //conversion formula to Celsius -- (num * 9/5 + 32)
+  return Math.round(((celsius * 9 / 5) + 32) *10)/10;
 };
+
+// console.log(convertToCelsius(32));
+// console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
