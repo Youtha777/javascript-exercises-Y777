71b0adf777b52b64ea46bd1c9a3589f6faae3ba0
Youtah <61557913+Youtha777@users.noreply.github.com>
2026-08-03 20:27:28 -0700

    Completed part of excercise Foundation 12-Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arry) {
+  return arry.reduce((total, currVal) => {
+    return (total += currVal);
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arry) {
+  return arry.reduce((total, currVal) => {
+    return (total *= currVal);
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  const arry = [];
+  for (i = b; i > 0; i--) {
+    arry.push(a);
+  }
+
+  return multiply(arry);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  const result = 1;
+  const arry = [];
+
+  for (let i = 1; i <= num; i++) {
+    arry.push(i);
+  }
+
+  return multiply(arry);
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
