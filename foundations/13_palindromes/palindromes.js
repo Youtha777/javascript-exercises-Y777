4570f6a0d5ddf4d0b0f51b5ee0416940c227da80
Youtharin Kuy <61557913+Youtha777@users.noreply.github.com>
2026-08-05 11:20:04 -0700

    Completed Foundation - 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  
+  const newStr = str.replace(/[^a-zA-Z0-9]/g, ''); 
+  let strLength = newStr.length - 1;
+  
+  console.log(newStr);
+
+  for (let i = 0; i < strLength; i++){
+    if (newStr[i].toLowerCase() != newStr[strLength].toLowerCase()){
+      // console.log(`${newStr[i].toLowerCase()}, ${i} and ${newStr[strLength].toLowerCase()}, ${strLength}`);
+      return false;
+    } else{
+      // console.log(`${newStr[i].toLowerCase()} and ${newStr[strLength].toLowerCase()}`)
+      strLength--;
+    }
+
+  }
+
+  return true;
 
 };
+
+// console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
